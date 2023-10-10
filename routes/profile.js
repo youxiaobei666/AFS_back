0e6080a22b96bfdca271594aaa6090273eedcf8f
YouJiangChuan <youxiaobei666@gmail.com>
2023-10-11 00:01:01 +0800

    friends

--- routes/profile.js
+++ routes/profile.js
@@ -31,6 +31,7 @@ router.post("/", (req, res) => {
   }
   // 定义查询所有用户的语句
   var getUserInfo = `SELECT * FROM  ${process.env.databaseName}.users where username = '${username}'`;
+  
   // 开始查询
   connection.query(getUserInfo, (err, queryRes) => {
     // 处理数据库错误
@@ -39,6 +40,39 @@ router.post("/", (req, res) => {
     }
     // 赋值给 users
     userInfo = queryRes[0];
+    // 查询用户的好友ID列表
+  var friendIDs = userInfo.friends.split(",").map((id) => parseInt(id, 10));
+
+  // 查询好友信息
+  var getFriendsInfo = `SELECT * FROM ${
+    process.env.databaseName
+  }.users WHERE id IN (${friendIDs.join(",")})`;
+
+  connection.query(getFriendsInfo, (err, friendsRes) => {
+    if (err) {
+      console.log(err);
+      return res.json({
+        success: false,
+        code: 500,
+        data: {
+          message: "服务器异常",
+        },
+      });
+    }
+
+    // 构建好友信息数组
+    const userFriendsList = friendsRes.map(friend => ({
+      id: friend.id,
+      username: friend.username,
+      friend_img: friend.img,
+      friend_city: friend.city,
+      friend_email: friend.email,
+      friend_age: friend.age
+    }));
+
+    // 将好友信息添加到userInfo中
+    userInfo.userFriendsList = userFriendsList;
+
     res.json({
       success: true,
       code: 200,
@@ -48,7 +82,8 @@ router.post("/", (req, res) => {
       },
     });
   });
- 
+})
+
 });
 
 // 导出这个路由
